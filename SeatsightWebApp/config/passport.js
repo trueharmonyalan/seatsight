10b95e271919b30cbbb86dac3c0dddde1d472ad0
Alan <attachmail.alan@gmail.com>
2025-02-04 23:27:48 +0530

    logout button setted for home page settings feature

--- SeatsightWebApp/config/passport.js
+++ SeatsightWebApp/config/passport.js
@@ -4,47 +4,38 @@ import argon2d from "argon2";
 
 import db from "./database.js";
 
+passport.use(
+  new Strategy(async function verify(usename, password, cb) {
+    try {
+      const result = await db.query("select * from users where email = $1", [
+        usename,
+      ]);
 
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHasH = user.password;
 
-
-
-
-passport.use(new Strategy(async function verify(usename,password,cb){
-
-    try{
-
-        const result = await db.query("select * from users where email = $1",[usename])
-
-        if(result.rows.length>0){
-            const user = result.rows[0]
-            const storedHasH = user.password
-
-            if(await argon2d.verify(storedHasH,password)){
-
-                return cb(null,user)
-            }else{
-                return cb(null,false)
-            }
-
-        }else{
-            console.log("user not found")
-            return cb("user not found")
+        if (await argon2d.verify(storedHasH, password)) {
+          return cb(null, user);
+        } else {
+          return cb(null, false);
         }
-
-    }catch(err){
-        console.error(err)
-        
+      } else {
+        console.log("user not found");
+        return cb("user not found");
+      }
+    } catch (err) {
+      console.error(err);
     }
-    
-}))
+  })
+);
 
-passport.serializeUser((user,cb)=>{
-    cb(null,user)
-})
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
 
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+});
 
-passport.deserializeUser((user,cb)=>{
-    cb(null,user)
-})
-
-export default passport
+export default passport;
--- SeatsightWebApp/routes/authentication.js
+++ SeatsightWebApp/routes/authentication.js
@@ -1,8 +1,8 @@
 // /routes/auth.js
 import express from "express";
 const router = express.Router();
-import argon2 from "argon2"
-import pg from "pg"
+import argon2 from "argon2";
+import pg from "pg";
 
 // file imports
 import db from "../config/database.js";
@@ -10,101 +10,86 @@ import passport from "passport";
 
 const currentYear = new Date().getFullYear();
 
-console.log()
-router.get("/auth",(req,res)=>{
-  if(req.isAuthenticated()){
-    res.render("home.ejs",{ 
+console.log();
+router.get("/auth", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("home.ejs", {
       year: currentYear,
-      state: true
-    })
-
-  }else{
-    res.redirect("/")
+      state: true,
+    });
+  } else {
+    res.redirect("/");
   }
-})
-
-
-
-router.get("/logout",(req,res)=>{
-  req.logout((err)=>{
-    if(err){
-      return err
-    }
-    res.redirect("/")
-  })
-
-})
-
-
-
-router.post("/register", async (req, res) => {
-    const email = req.body.username
-    const password = req.body.password
-
-    console.log(email)
-    console.log(password)
-
-    if(!req.body.username){
-      res.render("register.ejs",{
-        potentialError:{
-          noemailError: "Provide your email.",
-        }
-    })
-  }
-
-    if(!req.body.password){
-      res.render("register.ejs",{
-        potentialError:{
-          noPasswordError: "Provide your password.",
-        }
-    })
-  }
-
-  try{
-
-    const checkEmail = await db.query("select * from users where email = $1",[email])
-
-
-    if(checkEmail.rows.length>0){
-      res.render("register.ejs",{
-          potentialError:{
-            emailError: "This email is already registered with us.",
-          }
-      })
-      
-    }else{
-
-        const hash = await argon2.hash(password)
-        const result =await db.query("insert into users(email,password) values ($1,$2) RETURNING *",[email,hash])
-        
-        const user = result.rows[0]
-
-        req.login(user,(err)=>{
-          console.log(err)
-          res.redirect("/auth")
-        })
-
-       
-    }
-
-  }catch(err){
-    console.log(err)
-  }
-  
 });
 
+router.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return err;
+    }
+    res.redirect("/");
+  });
+});
 
+router.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
 
+  console.log(email);
+  console.log(password);
 
-router.post("/login",passport.authenticate("local",{
-  successRedirect: "/auth",
-  failureRedirect: "login",
-  
-}));
+  if (!req.body.username) {
+    res.render("register.ejs", {
+      potentialError: {
+        noemailError: "Provide your email.",
+      },
+    });
+  }
 
+  if (!req.body.password) {
+    res.render("register.ejs", {
+      potentialError: {
+        noPasswordError: "Provide your password.",
+      },
+    });
+  }
 
+  try {
+    const checkEmail = await db.query("select * from users where email = $1", [
+      email,
+    ]);
 
+    if (checkEmail.rows.length > 0) {
+      res.render("register.ejs", {
+        potentialError: {
+          emailError: "This email is already registered with us.",
+        },
+      });
+    } else {
+      const hash = await argon2.hash(password);
+      const result = await db.query(
+        "insert into users(email,password) values ($1,$2) RETURNING *",
+        [email, hash]
+      );
 
+      const user = result.rows[0];
 
+      req.login(user, (err) => {
+        console.log(err);
+        res.redirect("/auth");
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/auth",
+    failureRedirect: "login",
+  })
+);
 
 export default router;
