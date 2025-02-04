1af138aa6fcf9622199e000f7c8ecabe9d821354
Alan <attachmail.alan@gmail.com>
2025-02-04 23:27:48 +0530

    logout button setted for home page settings feature

--- SeatsightWebApp/routes/homepage-settings-card.js
+++ SeatsightWebApp/routes/homepage-settings-card.js
@@ -1,59 +1,37 @@
-import express from "express"
-import passport from "passport"
+import express from "express";
+import passport from "passport";
 
-
-const router = express.Router()
+const router = express.Router();
 
 const currentYear = new Date().getFullYear();
 
-router.get("/settings",(req,res)=>{
-    if(req.isAuthenticated()){
-        res.render("homepage-settings-card.ejs",
-            {
-                year: currentYear,
-                state: true
-            }
-        )
-    }else{
-        res.redirect("/")
+router.get("/settings", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("homepage-settings-card.ejs", 
+        {
+      year: currentYear,
     }
-    
-})
+);
+  } else {
+    res.redirect("/");
+  }
+});
 
+router.post("/settings", (req, res) => {
+  const url = req.body.ipurl;
+  console.log(url);
 
+  var urlActive = false;
+  if (url) {
+    urlActive = true;
+  }
 
-router.post("/settings",(req,res)=>{
-    const url = req.body.ipurl
-    console.log(url)
+  res.render("homepage-settings-card.ejs", {
+    info: {
+      showFromUrlState: urlActive,
+      showUrl: url,
+    },
+  });
+});
 
-    var urlActive = false
-    if(url){
-        urlActive = true
-    }
-
-    res.render("homepage-settings-card.ejs",{
-
-        info: {
-            showFromUrlState : urlActive,
-            showUrl: url
-
-        }
-        
-    }
-        
-    )
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default router
+export default router;
