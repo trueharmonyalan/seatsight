c6a284d7fb16df8f92fd891ab3e075f6b17fcfa5
Alan <attachmail.alan@gmail.com>
2025-02-13 15:03:17 +0530

    booked info displayed,back button added to it

--- SeatsightWebApp/routes/booked-seat-info.js
+++ SeatsightWebApp/routes/booked-seat-info.js
@@ -2,9 +2,20 @@
 import express from "express";
 const router = express.Router();
 
+const currentYear = new Date().getFullYear()
 
 router.get("/bookedInfo",(req,res)=>{
-    res.render("booked-seat-info.ejs")
+    if(req.isAuthenticated()){
+        res.render("booked-seat-info.ejs",{
+            year: currentYear,
+            info : {
+            totalSeats:4,
+            bookedSeats:1
+            }
+            
+        })
+    }
+    
 })
 
 
